1d9a1d08c7c2274b05155139e5104581256213cd
Onadebi <onadebi@onadebi.com>
2024-01-08 18:56:39 -0500

    Additioon of ChromeDriver and ChromeOptions

--- src/test/java/com/onaxsys/AppTest.java
+++ src/test/java/com/onaxsys/AppTest.java
@@ -1,10 +1,14 @@
 package com.onaxsys;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Unit test for simple App.
@@ -16,7 +20,13 @@ public class AppTest
     @BeforeEach
     public void setUp() {
         System.out.println("Setup - Initializing Chrome Driver");
-         _driver  = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--headless");
+        //get current directory
+        String currentDir = System.getProperty("user.dir");
+        String fullPath = Paths.get(currentDir, "drivers","X64", "chromedriver.exe").toString();
+        System.setProperty("webdriver.chrome.driver", fullPath);
+         _driver  = new ChromeDriver(options);
         _driver.get("https://www.google.com");
     }
     /**
