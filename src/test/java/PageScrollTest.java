57b051c61a2476b594595120bec7c572a8f0615f
Onadebi <onadebi@onadebi.com>
2024-02-02 14:44:47 -0500

    Scroll and Move between Tabs Test

--- src/test/java/PageScrollTest.java
+++ src/test/java/PageScrollTest.java
@@ -1,23 +1,28 @@
 import configs.BaseChromeTest;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.By;
 
 import java.nio.file.Paths;
 
+
 public class PageScrollTest extends BaseChromeTest {
 
     @Test
-    void inputKeyPress() throws InterruptedException{
+    void scrollToInputKeyPress() throws InterruptedException{
         _driver.navigate().to("https://formy-project.herokuapp.com/scroll");
-//        WebElement address = _driver.findElement(By.id("autocomplete"));
-//        address.click();
-//        address.sendKeys("1555 Park Blvd, Palo Alto, CA");
+
+        WebElement name  = _driver.findElement(By.xpath("//*[@id='name']"));
+//        #region May not be needed
+        Actions actions = new Actions(_driver);
+        actions.moveToElement(name);
+//        #endregion
+        name.sendKeys("Onadebi");
+
+        WebElement date = _driver.findElement(By.xpath("//*[@id='date']"));
+        date.sendKeys("02/02/2024");
 
         Thread.sleep(5000);
     }
--- src/test/java/SwitchToActiveWindowTest.java
+++ src/test/java/SwitchToActiveWindowTest.java
@@ -1,21 +1,31 @@
+import configs.BaseChromeTest;
 import  org.openqa.selenium.WebElement;
 import org.junit.jupiter.api.Test;
-import configs.BaseChromeTest;
 import org.openqa.selenium.By;
+import java.util.Set;
 
-public class SwitchToActiveWindow extends BaseChromeTest {
+public class SwitchToActiveWindowTest extends BaseChromeTest {
 
     @Test
     void switchToWindowTest() throws InterruptedException{
         _driver.navigate().to("https://formy-project.herokuapp.com/switch-window");
 
-        WebElement newTabButton = _driver.findElement(By.xpath("//*[@id='alert-button']"));
-        newTabButton.click();
+//        WebElement newTabButton = _driver.findElement(By.xpath("//*[@id='alert-button']"));
+//        newTabButton.click();
 
         WebElement btnNewTab = _driver.findElement(By.xpath("//*[@id='new-tab-button']"));
         btnNewTab.click();
 
+        Set<String> originalHandle = _driver.getWindowHandles();
+
         Thread.sleep(5000);
+        String firstWindow = originalHandle.iterator().next();
+        _driver.switchTo().window(firstWindow);
+
+        Thread.sleep(5000);
+
+        String initialWindow = originalHandle.stream().findFirst().orElse("");
+        _driver.switchTo().window(initialWindow);
     }
 
 }
